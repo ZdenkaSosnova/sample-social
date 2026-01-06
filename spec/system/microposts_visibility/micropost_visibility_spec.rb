928645991b38f1075ece26c05a3ef53eed943f0b
Zdenka Sosnova <zdendasosnova@gmail.com>
2026-01-06 12:35:54 +0100

    Initial commit with test suite for QA assessment

--- spec/system/microposts_visibility/micropost_visibility_spec.rb
+++ spec/system/microposts_visibility/micropost_visibility_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Micropost visibility', type: :system, signed_in: true do
-  let(:current_user) { create(:user, name: 'John Doe') }
+RSpec.describe 'Micropost visibility', type: :system do
+  let(:current_user) { create(:user, name: 'John Doe', password: 'password123') }
   let(:user) { create(:user) }
   let!(:micropost) { create(:micropost, user: user, content: 'Content') }
+
+  before do
+    visit login_path
+    fill_in 'Email', with: current_user.email
+    fill_in 'Password', with: 'password123'
+    click_button 'Log in'
+    expect(page).to have_content('John Doe')
+  end
 
   it 'user can take a look at other user feed' do
     visit user_path(user)
